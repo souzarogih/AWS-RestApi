ef25f5997b10f0c555b271730cf503ca14263beb
higorsouza <higor.andrade@hotmail.com>
2023-12-16 13:27:04 -0300

    Primeiros arquivos do projeto

---
+++ src/main/java/com/aws/course/AwsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.aws.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCourseApplication.class, args);
+    }
+
+}
